416460cc683dc2fbe7ede6e1e40ecbccfab02218
Mario_Stargazer <marioalvesneto@hotmail.com>
2023-03-30 23:55:55 -0300

    Remove baseurl content

--- src/FruitMarket.Client/src/App.jsx
+++ src/FruitMarket.Client/src/App.jsx
@@ -7,7 +7,7 @@ import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 import appLogo from './assets/WatermelonAmico.svg';
 
 function App() {
-  const baseURL = "https://fruit-market-api.azurewebsites.net/api/fruit-market"
+  const baseURL = ""
 
   const [data, setData] = useState([]);
   const [modalInclude, setModalInclude] = useState(false);
